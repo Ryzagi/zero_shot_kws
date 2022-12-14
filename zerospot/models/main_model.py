dfd4b1f333da386c62103f788fae042cd9114db8
Vladislav Ryzhov <vladislavrzhv@gmail.com>
2022-12-14 14:38:20 +0300

    feat: add new files for asr interface

---
+++ zerospot/models/main_model.py
@@ -0,0 +1,28 @@
+import torch
+from torch import nn
+
+from zerospot.models.model import BcResNetModel
+from zerospot.models.text_encoder import TextEncoder
+
+
+class MainModel(nn.Module):
+    def __init__(self, num_embeddings, hidden_dim, num_layers):
+        super(MainModel, self).__init__()
+        self.cnn_model = BcResNetModel(n_class=None)
+        self.text_encoder = TextEncoder(num_embeddings, hidden_dim, num_layers)
+        self.criterion = nn.BCEWithLogitsLossLoss()
+
+    def forward(self, text_indexes, text_lengths, spectrogram_features):
+        encoder_output, hidden = self.text_encoder(text_indexes, text_lengths)
+        cnn_model_output = self.cnn_model(spectrogram_features)
+        encoder_output = torch.mean(encoder_output, dim=1)
+        output = torch.bmm(encoder_output.unsqueeze(1), cnn_model_output.unsqueeze(2))
+        return output.squeeze()
+
+
+if __name__ == '__main__':
+    model = MainModel(4, 32, 1)
+    text_indexes = torch.LongTensor([[1, 2, 3], [1, 2, 0]])
+    text_lengths = torch.LongTensor([3, 2])
+    spectrogram_features = torch.randn(2, 1, 40, 128)
+    pred = model(text_indexes, text_lengths, spectrogram_features)
--- zerospot/models/text_encoder.py
+++ zerospot/models/text_encoder.py
@@ -1,6 +1,6 @@
 from torch import nn
-from torch.nn.utils.rnn import pack_padded_sequence
-
+from torch.nn.utils.rnn import pack_padded_sequence, pad_packed_sequence
+import torch
 
 class TextEncoder(nn.Module):
     def __init__(self, num_embeddings, hidden_dim, num_layers):
@@ -14,8 +14,14 @@ class TextEncoder(nn.Module):
         self.gru = nn.GRU(self.hidden_dim, self.hidden_dim, batch_first=True, num_layers=self.num_layers)
 
     def forward(self, input, input_len):
+
         embedded = self.embedding(input)
+        #embedded = embedded.transpose(1, 2)
         packed_embedded = pack_padded_sequence(embedded, input_len, batch_first=True)
+        #print(packed_embedded)
         packed_outputs, hidden = self.gru(packed_embedded)
-        return packed_outputs, hidden
+        unpacked_seq, unpacked_lens = pad_packed_sequence(packed_outputs, batch_first=True)
+        #outputs, _ = nn.utils.rnn.pad_packed_sequence(packed_outputs)
+        #hidden = torch.tanh(self.fc(torch.cat((hidden[-2, :, :], hidden[-1, :, :]), dim=1)))
+        return unpacked_seq, hidden
 
