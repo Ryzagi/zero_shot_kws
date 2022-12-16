ddf61f69cfbfd9ccc69112883775773a049e0e88
Vladislav Ryzhov <vladislavrzhv@gmail.com>
2022-12-16 18:15:48 +0300

    feat: add tokenizer

---
+++ zerospot/data/tokenizer.py
@@ -0,0 +1,34 @@
+from pathlib import Path
+import json
+
+
+class Tokenizer:
+    def __init__(self):
+        self.tokens = json.loads((Path(__file__) / 'tokens.json').read_text())
+        self.pad_token_id = 0
+
+    def __call__(self, word_list):
+
+        tokens_list_ids = []
+        tokens_list_lentghts = []
+
+        max_length = max([len(word) for word in word_list])
+
+        for word in word_list:
+            tokens_ids = []
+            tokens_lentghts = []
+
+            for char in word:
+                token_id = self.tokens[char]
+                tokens_ids.append(token_id)
+
+            token_length = len(tokens_ids)
+
+            padding = [self.pad_token_id] * (max_length - token_length)
+
+            tokens_ids.extend(padding)
+            tokens_lentghts.append(token_length)
+            tokens_list_ids.append(tokens_ids)
+            tokens_list_lentghts.extend(tokens_lentghts)
+
+        return tokens_list_ids, tokens_list_lentghts
